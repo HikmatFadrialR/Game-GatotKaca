aebcf8f5b2db92414f6028f560a770011452b091
HikmatFadrialR <hikmatfadrial1012@gmail.com>
2022-04-21 10:56:46 +0700

    First

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float jumpForce;
+    public float movespeed;
+    bool facingRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerJump();
+        FlipTrigger();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerController();
+    }
+
+    void PlayerController()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+
+        Vector3 movement = new Vector3(x * movespeed, rb.velocity.y, 0f);
+        rb.velocity = movement;
+    }
+
+    void FlipTrigger()
+    {
+        if (rb.velocity.x < 0 && facingRight)
+        {
+            FlipPlayer();
+        }
+        else if (rb.velocity.x > 0 && !facingRight)
+        {
+            FlipPlayer();
+        }
+    }
+
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+
+    void PlayerJump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, 0f);
+        }
+    }
+}
---
+++ Assets/Script/SceneHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Application.LoadLevel(0);
+    }
+
+    private void OnMouseUp()
+    {
+
+    }
+}
