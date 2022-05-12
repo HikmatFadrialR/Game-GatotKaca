2b1e02f04019b92c6d0a8941ff19c027f85561f4
HikmatFadrialR <hikmatfadrial1012@gmail.com>
2022-05-12 11:40:20 +0700

    Menambahkan Score

---
+++ Assets/Script/cherry.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cherry : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("GatotKaca"))
+        {
+            print("cherry diambil");
+            Destroy(gameObject);
+            cherryscore.hitungCherry += 50;
+        }
+    }
+}
---
+++ Assets/Script/cherryscore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cherryscore : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static int hitungCherry;
+    Text hitungCherryText;
+
+    void Start()
+    {
+        hitungCherry = 0;
+        hitungCherryText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hitungCherryText.text = hitungCherry.ToString();
+    }
+}
